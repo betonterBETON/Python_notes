029f3467125c91f0b80f1b41df1ea5d5c728f25a
betonterBETON <67642201+betonterBETON@users.noreply.github.com>
2021-05-25 12:14:18 +0200

    Notes p-core words.py changes

--- Modularity/words.py
+++ Modularity/words.py
@@ -20,16 +20,10 @@ def print_items(items):  # this will print ANY iterable collection
         print(item)
 
 
-def main():  # by putting main() in a separate function,
-    # we can test the main block in the REPL (otherwise, we cound't call it from there,
-    # if this block only appeared after the if-name-main idiom!)
-    # then, in the REPL, do: "from words import main", and you can run it.
-    url = sys.argv[1]  # argv is a list of strings, which make the cmd arguments!
-    # this makes it unable to test from the REPL
-    # via (import * main()) or even (import * main() http....txt) !!!
+def main(url):
     words = fetch_words(url)  # 'http://sixty-north.com/c/t.txt'
-    print_items(words)  # to get appropriate result, run: python3 words.py http://sixty-north.com/c/t.txt
+    print_items(words)  # now we can test in REPL, by (import *, main('htttp...txt')
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])  # ... and also it will work when executed with arg in cmd line
