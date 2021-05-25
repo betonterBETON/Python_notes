9d80c31a654fd908946d66ab598727ecfdc75663
betonterBETON <67642201+betonterBETON@users.noreply.github.com>
2021-05-25 11:21:34 +0200

    Notes p-core words.py changes

--- Modularity/words.py
+++ Modularity/words.py
@@ -1,8 +1,9 @@
+import sys  # lets us accept command line arguments
 from urllib.request import urlopen
 
 
-def fetch_words():
-    story = urlopen('http://sixty-north.com/c/t.txt')
+def fetch_words(url):
+    story = urlopen(url)
     story_words = []
     for line in story:
         line_words = line.decode('utf8').split()
@@ -12,20 +13,20 @@ def fetch_words():
     return story_words
 
 
-def print_words(story_words):  # this will print ANY iterable collection
+def print_items(items):  # this will print ANY iterable collection
     # such as a string, one char per line...
     # or such as a list of numbers
-    # so "print words" isn't a great name, it's misleading
-    for word in story_words:
-        print(word)
+    for item in items:
+        print(item)
 
 
 def main():     # by putting main() in a separate function,
     # we can test the main block in the REPL (otherwise, we cound't call it from there,
     # if this block only appeared after the if-name-main idiom!)
     # then, in the REPL, do: "from words import main", and you can run it.
-    words = fetch_words()
-    print_words(words)
+    url = sys.argv[1]   # argv is a list of strings, which make the cmd arguments!
+    words = fetch_words(url)    # 'http://sixty-north.com/c/t.txt'
+    print_items(words)  # to get appropriate result, run: python3 words.py http://sixty-north.com/c/t.txt
 
 
 if __name__ == '__main__':
