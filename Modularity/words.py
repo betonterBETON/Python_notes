21d257db194ffe966ab8a6ac7b116cf33c7de124
betonterBETON <67642201+betonterBETON@users.noreply.github.com>
2021-05-25 09:04:21 +0200

    Notes p-core execution model

--- Modularity/words.py
+++ Modularity/words.py
@@ -1,8 +1,7 @@
 from urllib.request import urlopen
 
 
-def fetch_words():  # now to run this code, first import words, then run words.fetch_words()
-                    # or from words import fetch_words
+def fetch_words():
     story = urlopen('http://sixty-north.com/c/t.txt')
     story_words = []
     for line in story:
@@ -10,16 +9,19 @@ def fetch_words():  # now to run this code, first import words, then run words.f
         for word in line_words:
             story_words.append(word)
     story.close()
+    return story_words
+
+
+def print_words(story_words):  # this will print ANY iterable collection
+    # such as a string, one char per line...
     for word in story_words:
         print(word)
 
-# ^ this is not executable, that means, if called by python words.py in cmd, it will not yield
-# any results!
 
-print(__name__)  # this will produce "__main__" when executed as "python words.py"from shell
-# and will produce "words" if imported (only for the first import, if it's done again,
-# nothing will be printed. CODE EXECUTED ONLY ONCE AT 1ST IMPORT
+def main():
+    words = fetch_words()
+    print_words(words)
 
-if __name__ == '__main__':  # this will only run when executed as script (called from shell)
-    # it will NOT run when imported
-    fetch_words()
+
+if __name__ == '__main__':
+    main()
